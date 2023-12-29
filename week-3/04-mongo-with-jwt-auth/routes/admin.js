6a00d48abef352a8d2b1c56384bfb626df832239
Chirag Jain <cj27chirag@gmail.com>
2023-12-30 00:07:35 +0530

    Added jwt library

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const secretKey = "123456"
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+
 });
 
 router.post('/signin', (req, res) => {
